f3dd04be7e61bd8ec75d9a20ac8549d08e88683b
KawanNasc <atlaskawan@gmail.com>
2024-05-10 22:22:23 -0300

    CRUD da API completa no PostMan

--- app/Http/Controllers/MotoController.php
+++ app/Http/Controllers/MotoController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Moto;
 use Illuminate\Http\Request;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
 
@@ -19,7 +18,7 @@ class MotoController extends Controller
 
         $contador = $motos->count();
 
-        return 'N° de Motos: ' . $contador . '<br>'.  $motos.Response()->json([], Response::HTTP_NO_CONTENT);
+        return 'N° de Motos: ' . $contador . '<br>' .  $motos . Response()->json([], Response::HTTP_NO_CONTENT);
     }
 
     /**
@@ -27,44 +26,86 @@ class MotoController extends Controller
      */
     public function store(Request $request)
     {
-        $motos = $request::All();
+        $motos = $request->All();
 
         $validacao = Validator::make($motos, [
             'marca' =>  'required',
             'modelo' =>  'required',
-            'cor' =>  'required', 
+            'cor' =>  'required',
             'ano' =>  'required'
         ]);
 
-        if ($validacao->fails()) { return 'Dados inválidos' . $validacao->error(true) . 500; }
+        if ($validacao->fails()) {
+            return 'Dados inválidos' . $validacao->error(true) . 500;
+        }
 
         $cadastro = Moto::create($motos);
 
-        if ($cadastro) { return 'Dados cadastrados com sucesso' . Response()->json([], Response::HTTP_NO_CONTENT); }
-        else { return 'Dados não cadastrados' . Response()->json([], Response::HTTP_NO_CONTENT); }
+        if ($cadastro) {
+            return 'Dados cadastrados com sucesso'  . Response()->json([], Response::HTTP_NO_CONTENT);
+        } else {
+            return 'Dados não cadastrados ' . Response()->json([], Response::HTTP_NO_CONTENT);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Moto $moto)
+    public function show(string $id)
     {
-        //
+        $motos = Moto::find($id);
+
+        if ($motos) {
+            return 'Moto localizada ' . $motos;
+        } else {
+            return 'Moto não localizada ' . Response()->json([], Response::HTTP_NO_CONTENT);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Moto $moto)
+    public function update(Request $request, string $id)
     {
-        //
+        $motos = $request->all();
+
+        $validacao = Validator::make($motos, [
+            'marca' =>  'required',
+            'modelo' =>  'required',
+            'cor' =>  'required',
+            'ano' =>  'required'
+        ]);
+
+        if ($validacao->fails()) {
+            return 'Dados inválidos' . $validacao->error(true) . 500;
+        }
+
+        $alteracao = Moto::find($id);
+        $alteracao->marca = $motos['marca'];
+        $alteracao->modelo = $motos['modelo'];
+        $alteracao->cor = $motos['cor'];
+        $alteracao->ano = $motos['ano'];
+
+        $retorno = $alteracao->save();
+
+        if ($retorno) {
+            return 'Dados atualizados com sucesso ' . Response()->json([], Response::HTTP_NO_CONTENT);
+        } else {
+            return 'Dados não atualizados ' . Response()->json([], Response::HTTP_NO_CONTENT);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Moto $moto)
+    public function destroy(string $id)
     {
-        //
+        $motos = Moto::find($id);
+
+        if ($motos->delete()) {
+            return 'A moto foi deletada ' . response()->json([], Response::HTTP_NO_CONTENT);
+        }
+
+        return 'A moto não foi deletada ' . response()->json([], Response::HTTP_NO_CONTENT);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Response;
 use App\Http\Controllers\MotoController;
 
-Route::get('/', function() { return response()->json(['Sucesso' => true]); });
+Route::get('/', function () {
+    return response()->json(['Sucesso' => true]);
+});
 
 Route::get('/motos', [MotoController::class, 'index']);
-
-Route::post('/cadastroMotos', [MotoController::class, 'store']);
+Route::get('/motos/{id}', [MotoController::class, 'show']);
+Route::post('/motos', [MotoController::class, 'store']);
+Route::put('/motos/{id}', [MotoController::class, 'update']);
+Route::delete('/motos/{id}', [MotoController::class, 'destroy']);
